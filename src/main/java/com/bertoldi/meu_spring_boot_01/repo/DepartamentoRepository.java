ed80621de462d9d56087238baf1956e06754d744
Cleiton Bertoldi <cleitonbertoldi78@gmail.com>
2025-10-23 20:49:07 -0300

    Adicionando Anotações

--- src/main/java/com/bertoldi/meu_spring_boot_01/repo/DepartamentoRepository.java
+++ src/main/java/com/bertoldi/meu_spring_boot_01/repo/DepartamentoRepository.java
@@ -1,4 +1,11 @@
 package com.bertoldi.meu_spring_boot_01.repo;
 
-public class DepartamentoRepository {
+import com.bertoldi.meu_spring_boot_01.entity.Departamentoentity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartamentoRepository extends JpaRepository <Departamentoentity,Integer> {
+
+
+
+
 }
