2227c57c56d3c478712c69c8806b9085a8f0fe89
Cleiton Bertoldi <cleitonbertoldi78@gmail.com>
2025-10-28 21:23:51 -0300

    Atualizando o FuncionarioService

--- src/main/java/com/bertoldi/meu_spring_boot_01/services/FuncionarioService.java
+++ src/main/java/com/bertoldi/meu_spring_boot_01/services/FuncionarioService.java
@@ -1,49 +1,42 @@
 package com.bertoldi.meu_spring_boot_01.services;
 
 import com.bertoldi.meu_spring_boot_01.dto.DepartamentoDto;
+import com.bertoldi.meu_spring_boot_01.dto.FuncionarioDto;
 import com.bertoldi.meu_spring_boot_01.entity.DepartamentoEntity;
+import com.bertoldi.meu_spring_boot_01.entity.FuncionarioEntity;
 import com.bertoldi.meu_spring_boot_01.repo.DepartamentoRepository;
+import com.bertoldi.meu_spring_boot_01.repo.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class FuncionarioService {
 
     @Autowired
+    private FuncionarioRepository funcionarioRepo;
+
+
+    @Autowired
     private DepartamentoRepository departamentoRepo;
-
-    //Crud
-
-
-
-
-
     //Create
 
-    //Conversão de dto para entity
-    public void cadastrarDepartamento(DepartamentoDto departamentoDto){
-        DepartamentoEntity departamentoEntity = new DepartamentoEntity();
+    public void cadastrarFuncionario(FuncionarioDto funcionarioDto) {
 
-        departamentoEntity.setNm_departamento(departamentoDto.getNmDepartamento());
+     Optional <DepartamentoEntity> departamento= departamentoRepo.findById(funcionarioDto.getDepartamento());
 
-        departamentoRepo.save(departamentoEntity);
+        FuncionarioEntity funcionarioEntity = new FuncionarioEntity();
+
+        funcionarioEntity.setNome(funcionarioDto.getNome());
+        funcionarioEntity.setEmail(funcionarioDto.getEmail());
+        funcionarioEntity.setSenha(funcionarioDto.getSenha());
+        funcionarioEntity.setSalario(funcionarioDto.getSalario());
+
+
+        funcionarioRepo.save(funcionarioEntity);
+        
+
+
     }
-
-
-
-
-
-    //Read
-
-
-
-    //Update
-
-
-
-
-    //Delete
-
-
-
 }
